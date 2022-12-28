f9cd5f6c6ccbc11f3e31761909c1cf0b8c11ba10
crimsonnaltep <tepelikyanalex@gmail.com>
2022-12-29 01:41:34 +0400

    Adding MUI

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import Days from "./components/Days";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
-const data = {
-  latitude: 40.1875,
-  longitude: 44.5,
-  generationtime_ms: 0.6879568099975586,
-  utc_offset_seconds: 14400,
-  timezone: "Asia/Yerevan",
-  timezone_abbreviation: "+04",
-  elevation: 989.0,
-  daily_units: {
-    time: "iso8601",
-    weathercode: "wmo code",
-    temperature_2m_min: "°C",
-  },
-  daily: {
-    time: [
-      "2022-12-28",
-      "2022-12-29",
-      "2022-12-30",
-      "2022-12-31",
-      "2023-01-01",
-      "2023-01-02",
-      "2023-01-03",
-    ],
-    weathercode: [3, 3, 1, 0, 1, 1, 0],
-    temperature_2m_min: [-3.1, -2.4, -3.0, -3.8, -4.0, -4.6, -3.5],
-  },
-};
+import data from "./components/Data";
 
 function App() {
   const obj = {
@@ -53,12 +26,13 @@ function App() {
   // useEffect(() => {
   //   axios.get(url).then(response => setData(response.data));
   // }, []);
+  console.log(data);
   return (
     <div>
       <h1 className="background">5-Day forecast</h1>
       <p className="location">Berlin</p>
       <div className="mainBox">
-        {daysOfWeek.map((day, key) => {
+        {/* {daysOfWeek.map((day, key) => {
           return (
             <Days
               key={key}
@@ -67,7 +41,7 @@ function App() {
               temp={data.daily.temperature_2m_min[key]}
             />
           );
-        })}
+        })} */}
         <p>{}</p>
       </div>
     </div>
