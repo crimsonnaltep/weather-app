f5dcc434c99c0cb693b48eb07b379265cd6e9fc0
crimsonnaltep <tepelikyanalex@gmail.com>
2022-12-28 18:31:33 +0400

    Trying to add MUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Days from "./components/Days";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+
+const data = {
+  latitude: 40.1875,
+  longitude: 44.5,
+  generationtime_ms: 0.6879568099975586,
+  utc_offset_seconds: 14400,
+  timezone: "Asia/Yerevan",
+  timezone_abbreviation: "+04",
+  elevation: 989.0,
+  daily_units: {
+    time: "iso8601",
+    weathercode: "wmo code",
+    temperature_2m_min: "°C",
+  },
+  daily: {
+    time: [
+      "2022-12-28",
+      "2022-12-29",
+      "2022-12-30",
+      "2022-12-31",
+      "2023-01-01",
+      "2023-01-02",
+      "2023-01-03",
+    ],
+    weathercode: [3, 3, 1, 0, 1, 1, 0],
+    temperature_2m_min: [-3.1, -2.4, -3.0, -3.8, -4.0, -4.6, -3.5],
+  },
+};
 
 function App() {
   const obj = {
@@ -12,54 +38,37 @@ function App() {
       xiar: 3,
     },
   };
-  const [data, setData] = useState();
-  const url =
-    "https://api.open-meteo.com/v1/forecast?latitude=40.18&longitude=44.51&daily=weathercode,temperature_2m_min&timezone=auto";
-  useEffect(() => {
-    axios.get(url).then(response => setData(response.data));
-  }, []);
-  console.log(data);
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Thursday",
+    "Wednsday",
+    "Tuesday",
+    "Friday",
+    "Saturday",
+  ];
+  // const [data, setData] = useState();
+  // const url =
+  //   "https://api.open-meteo.com/v1/forecast?latitude=40.18&longitude=44.51&daily=weathercode,temperature_2m_min&timezone=auto";
+  // useEffect(() => {
+  //   axios.get(url).then(response => setData(response.data));
+  // }, []);
   return (
     <div>
       <h1 className="background">5-Day forecast</h1>
       <p className="location">Berlin</p>
       <div className="mainBox">
-        <Days
-          date={data.daily.time[0]}
-          dayName="Sunday"
-          temp={data.daily.temperature_2m_min[0]}
-        />
-        <Days
-          date={data.daily.time[1]}
-          dayName="Monday"
-          temp={data.daily.temperature_2m_min[1]}
-        />
-        <Days
-          date={data.daily.time[2]}
-          dayName="Thursday"
-          temp={data.daily.temperature_2m_min[2]}
-        />
-        <Days
-          date={data.daily.time[3]}
-          dayName="Wednsday"
-          temp={data.daily.temperature_2m_min[3]}
-        />
-        <Days
-          date={data.daily.time[4]}
-          dayName="Tuesday"
-          temp={data.daily.temperature_2m_min[4]}
-        />
-        <Days
-          date={data.daily.time[5]}
-          dayName="Friday"
-          temp={data.daily.temperature_2m_min[5]}
-        />
-        <Days
-          date={data.daily.time[6]}
-          dayName="Saturday"
-          temp={data.daily.temperature_2m_min[6]}
-        />
-        <p>{data.daily.temperature_2m_min[1]}</p>
+        {daysOfWeek.map((day, key) => {
+          return (
+            <Days
+              key={key}
+              date={data.daily.time[key]}
+              dayName={day}
+              temp={data.daily.temperature_2m_min[key]}
+            />
+          );
+        })}
+        <p>{}</p>
       </div>
     </div>
   );
--- src/components/Days.js
+++ src/components/Days.js
@@ -1,18 +1,14 @@
-import React, { Component } from "react";
 import "./Days.css";
-class Days extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="weatherBox">
-        <h3>{this.props.dayName}</h3>
-        <p>{this.props.date}</p>
-        <img></img>
-        <h1>{this.props.temp}</h1>
-        <p>Clear sky</p>
-      </div>
-    );
-  }
+function Days(props) {
+  return (
+    <div className="weatherBox">
+      <h3>{props.dayName}</h3>
+      <p>{props.date}</p>
+      <img></img>
+      <h1>{props.temp}</h1>
+      <p>Clear sky</p>
+    </div>
+  );
 }
 
 export default Days;
